de9dc3b9a38d7c66f1d1cc6b4d232e54100ab1db
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-02 18:08:08 +0000

    initialising the repository of the package with the init file

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+this package just includes a list of socket function that are really necessary for me
+to start creating and crafting packets.
+-------------------------------------------------------------------------------------
+"""
+from ip.connect import IP, this_ip
