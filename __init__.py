fc8cbe96fe02e606f3a35ca8d10e426c21ec0d7f
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-04 16:38:56 +0000

    updated this __init__ file provide a simple interface for importing modules

--- __init__.py
+++ __init__.py
@@ -4,3 +4,6 @@ the main entry point of the ip package is the __init__ file.
 specifies a few dependencies for the app and coagulates all the
 several pieces into one for easy imports.
 """
+
+from ip.resolve.resolve import Resolve
+from ip.utils.local import localhost
