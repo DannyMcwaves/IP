49552904a8a0e98d2ba19e5ca995642b6d5a2b35
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-04 16:09:01 +0000

    resolves various features about a particular ip address or hostname

---
+++ resolve/resolve.py
@@ -0,0 +1,8 @@
+"""
+this sub package is actually a dependency required by the main
+module directly in the IP package.
+
+it resolves the names of IP addresses. Checks the state or return the IP
+address of a particular hostname and vice versa.
+Among other function I will define later.
+"""
