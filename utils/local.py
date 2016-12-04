a6a09f1d8fb2da93a76122c64add85960780f973
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-04 18:54:48 +0000

    some few refactors to the local module

--- utils/local.py
+++ utils/local.py
@@ -11,30 +11,44 @@ import sys
 import re
 import os
 
+__all__ = ["localhost"]
+
 
 class localhost:
 
-    def __init__(iface=None):
+    def __init__(self):
+        """
+        the localhost does not even need any addresses passed. It is static.
+        """
+        self.__ip = "127.0.0.1"
+        self.__hostname = "localhost"
+
+    @property
+    def ipAddress(self, iface=None):
+        """
+        :return: A dictionary of all the ip addresses on the various interface of the localhost
+        """
         if iface is None:
             command = "ipconfig" if sys.platform.lower() == "windows" else "ifconfig"
         else:
             command = "ipconfig " + iface if sys.platform.lower() == "windows" else "ifconfig " + iface
-        addrs = re.findall(r"addr:\d+\.\d+\.\d+\.\d+", os.popen(command).read(-1))
 
-        if len(addrs) == 3:
+        addresses = re.findall(r"addr:\d+\.\d+\.\d+\.\d+", os.popen(command).read(-1))
+
+        if len(addresses) == 3:
             return {
-                "local": addrs[0][5:],
-                "wifi": addrs[2][5:],
-                "other": addrs[1][5:]
+                "local": addresses[0][5:],
+                "wifi": addresses[2][5:],
+                "other": addresses[1][5:]
             }
-        elif len(addrs) == 4:
+        elif len(addresses) == 4:
             return {
-                "modem/wired": addrs[0][5:],
-                "local": addrs[1][5:],
-                "wifi": addrs[3][5:],
-                "other": addrs[2][5:]
+                "modem/wired": addresses[0][5:],
+                "local": addresses[1][5:],
+                "wifi": addresses[3][5:],
+                "other": addresses[2][5:]
             }
         try:
-            return addrs[0][5:]
+            return addresses[0][5:]
         except IndexError:
             return NotImplemented
