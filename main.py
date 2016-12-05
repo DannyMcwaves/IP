9a50ff1b459280bc36f440fe9e305c807e472a0b
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-05 12:06:56 +0000

    performed defined tests over here

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+this where I run almost all tests defined in the test cases.
+"""
+
+from ip.tests import ResolveTestCase
+from ip.tests import IPTestCase
+from ip.tests import LocalhostTestCase
+
+if __name__ == '__main__':
+    print("T E S T . . . C A S E")
