99f948957bfdd39ea8ccf527d085a3b808301b61
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-05 08:47:28 +0000

    setup script easy installation on localhost

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="ip",
+    version="1.1.0",
+    author="Danny Mcwaves",
+    author_email="dannymcwaves96@gmail.com",
+    url="http://github.com/DannyMcwaves/IP",
+    packages=["ip"]
+)
