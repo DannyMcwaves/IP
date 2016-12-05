e2a7c3f6ba733d0cd96cd70fe7acb6d5711ede52
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-05 09:31:11 +0000

    adding a test case for all modules

---
+++ tests.py
@@ -0,0 +1,8 @@
+"""
+this contains at least three base classes that are used to test for the
+various classes available in this package.
+
+TESTS should be provided for Resolve, IP, and Localhost
+"""
+from unittest import TestCase, TestSuite
+
