84178af81555903e18e873db833e9d7af5a583cd
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-07 17:32:04 +0000

    completed setup for the package

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from distutils.core import setup
 
 setup(
     name="ip",
-    version="1.1.0",
-    author="Danny Mcwaves",
-    license="BSD",
+    version="1.0.0",
+    packages=["ip", "ip/utils", "ip/tests", "ip/resolve"],
+    author="Danny mcwaves",
     author_email="dannymcwaves96@gmail.com",
-    url="http://github.com/DannyMcwaves/IP",
-    packages=["tests", "resolve", "util"]
+    url="https://github.com/DannyMcwaves/IP",
+    description="A simple IP manipulation tool for ip addresses",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
 
