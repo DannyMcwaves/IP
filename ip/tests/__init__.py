561840e3ae57551ca4829c44ff862e27296713f9
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-07 17:38:24 +0000

    exposing the major apis for the test case

--- ip/tests/__init__.py
+++ ip/tests/__init__.py
@@ -1,9 +1,8 @@
+
 """
 Initializing the Test case for all the modules/packages in this Package.
 """
 
-from ip.tests import ResolveTestCase
-from ip.tests import LocalhostTestCase
 from ip.tests.tests import IPTestCase
 
-__all__ = ["ResolveTestCase", "LocalhostTestCase", "IPTestCase"]
+__all__ = ["IPTestCase"]
